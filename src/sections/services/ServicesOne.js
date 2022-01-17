dec364ecbd8997eca1e6aa05fe050eb6ddea9c8a
Daniel Pittman <dpdanpittman@gmail.com>
2022-01-16 22:02:57 -0800

    added numbers

--- src/sections/services/ServicesOne.js
+++ src/sections/services/ServicesOne.js
@@ -258,42 +258,42 @@ export default props => (
             }
           }
         }
-        web: file(relativePath: {eq: "icons/web.png"}) {
+        web: file(relativePath: {eq: "icons/1.png"}) {
           childImageSharp {
             fluid(maxWidth: 500) {
               src
             }
           }
         }
-        mobile: file(relativePath: {eq: "icons/mobile.png"}) {
+        mobile: file(relativePath: {eq: "icons/2.png"}) {
           childImageSharp {
             fluid(maxWidth: 500) {
               src
             }
           }
         }
-        seo: file(relativePath: {eq: "icons/seo2.png"}) {
+        seo: file(relativePath: {eq: "icons/3.png"}) {
           childImageSharp {
             fluid(maxWidth: 500) {
               src
             }
           }
         }
-        email: file(relativePath: {eq: "icons/email.png"}) {
+        email: file(relativePath: {eq: "icons/4.png"}) {
           childImageSharp {
             fluid(maxWidth: 500) {
               src
             }
           }
         }
-        ui: file(relativePath: {eq: "icons/sketch.png"}) {
+        ui: file(relativePath: {eq: "icons/5.png"}) {
           childImageSharp {
             fluid(maxWidth: 500) {
               src
             }
           }
         }
-        network: file(relativePath: {eq: "icons/network.png"}) {
+        network: file(relativePath: {eq: "icons/6.png"}) {
           childImageSharp {
             fluid(maxWidth: 500) {
               src
