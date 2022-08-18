828756b54afc6a3968ecf9b30382df3b1fb1fa04
Daniel Pittman <dpdanpittman@gmail.com>
2022-08-18 14:05:26 -0700

    Update index.js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -40,6 +40,7 @@ class Footer extends React.Component{
                     <FontAwesomeIcon icon={faTwitter} className="social_icon" onClick={() => window.open('https://www.twitter.com/CoHo_Cosmos')} />
                     <FontAwesomeIcon icon={faYoutube} className="social_icon" onClick={() => window.open('https://www.youtube.com')} />
                 </SocialIcons>
+                <br/>
                 <Text>
                 © 2022 Qwoyn Studios S-LLC, All rights reserved.
                </Text>
