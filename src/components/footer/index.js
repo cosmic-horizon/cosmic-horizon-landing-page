cf4673fd03c802b7986c08aaac83240309e16124
Daniel Pittman <dpdanpittman@gmail.com>
2022-08-18 14:42:23 -0700

    Update index.js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -40,11 +40,11 @@ class Footer extends React.Component{
                     <FontAwesomeIcon icon={faTwitter} className="social_icon" onClick={() => window.open('https://www.twitter.com/CoHo_Cosmos')} />
                     <FontAwesomeIcon icon={faYoutube} className="social_icon" onClick={() => window.open('https://www.youtube.com')} />
                 </SocialIcons>
-                <br/><br/>
-                <Text>
-                © 2022 Qwoyn Studios S-LLC, All rights reserved.
-               </Text>
             </FooterMain>
+            <br/><br/>
+            <Text>
+            © 2022 Qwoyn Studios S-LLC, All rights reserved.
+            </Text>            
         )
     }
 }
